94dfc8018f165d24e257d225ba76940aae21b0a5
Yoshkuni Kuniumi <101183804+yoshikuniii@users.noreply.github.com>
2023-10-10 18:53:53 +0700

    bump version 0.1.55 to 0.1.56

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.55'
+VERSION = '0.1.56'
 DESCRIPTION = 'Yet simple API wrapper for GoGoAnime'
 LONG_DESCRIPTION = 'PyNime is a (simple) straightforward Python3 script to scrape GoGoAnime using Python. The project ' \
                    'is a work in progress, not finished yet. But, the code works well, feel free to take part of the ' \
@@ -17,7 +17,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=['beautifulsoup4', 'requests', 'pycryptodome', 'm3u8', 'lxml', 'cssselect', 'yarl'],
+    install_requires=['beautifulsoup4', 'requests', 'pycryptodome', 'm3u8', 'lxml', 'cssselect', 'yarl', 'certifi'],
     keywords=['python', 'downloader', 'anime', 'webscrapping', 'beautifulsoup4', 'gogoanime', 'gogoanime-scraper'],
     classifiers=[
         "Programming Language :: Python :: 3",
